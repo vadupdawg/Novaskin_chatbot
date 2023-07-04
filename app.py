0a11bcf93c7c23a8174a7fda43f7f462f97b4e26
vadupdawg <niels1214@gmail.com>
2023-07-04 10:43:46 +0200

    finale update novaskin

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain.chains import ConversationalRetrievalChain
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 pinecone.init(
-    api_key = os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
-    environment = os.getenv("PINECONE_ENV"),  # next to api key in console
+    api_key = os.getenv("PINECONE_API_KEY"),
+    environment = os.getenv("PINECONE_ENV"), 
 )
 index_name = "novaskin"
 
