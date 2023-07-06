16c8bcb70c38567edd4dfb7182b7288589836766
vadupdawg <niels1214@gmail.com>
2023-07-06 15:19:58 +0200

    memory upgrade

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain.chains import ConversationalRetrievalChain
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 pinecone.init(
-    api_key = os.getenv("PINECONE_API_KEY"),
-    environment = os.getenv("PINECONE_ENV"), 
+    api_key = os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
+    environment = os.getenv("PINECONE_ENV"),  # next to api key in console
 )
 index_name = "novaskin"
 
@@ -33,11 +33,9 @@ logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
 chat_histories = {}
 
 # create a memory object
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-# Adding initial chat history
-initial_message = "Je bent de chatbot van NovaSkin, een bedrijf dat gespecialiseerd is in huidverzorging. Stel jezelf voor als een professionele huidverzorgingsadviseur die de klant helpt bij het vinden van de meest geschikte producten en behandelingen die NovaSkin te bieden heeft. Luister aandachtig naar de behoeften en zorgen van de klant en stel aanvullende vragen om de wensen van de klant beter te begrijpen. Wees altijd positief, beleefd en ondersteunend in je communicatie. Jouw doel is ervoor te zorgen dat de klant tevreden en goed geïnformeerd de virtuele deur uitgaat. Hoewel je veel weet, baseer je jouw advies alleen op de informatie en producten die beschikbaar zijn bij NovaSkin en vermijd je alle verwijzingen naar concurrenten of andere bronnen."
-memory.chat_memory.messages.append((memory.ai_prefix, initial_message))
+memory = ConversationBufferMemory(return_messages=True)
+memory.save_context({"input": "Je bent de chatbot van NovaSkin, een bedrijf dat gespecialiseerd is in huidverzorging. Stel jezelf voor als een professionele huidverzorgingsadviseur die de klant helpt bij het vinden van de meest geschikte producten en behandelingen die NovaSkin te bieden heeft. Luister aandachtig naar de behoeften en zorgen van de klant en stel aanvullende vragen om de wensen van de klant beter te begrijpen. Wees altijd positief, beleefd en ondersteunend in je communicatie. Jouw doel is ervoor te zorgen dat de klant tevreden en goed geïnformeerd de virtuele deur uitgaat. Hoewel je veel weet, baseer je jouw advies alleen op de informatie en producten die beschikbaar zijn bij NovaSkin en vermijd je alle verwijzingen naar concurrenten of andere bronnen."
+}, {"output": "Duidelijk, ik hou mij volledig aan de wensen van NovaSkin en ben altijd positief, beleefd en ondersteunend in mijn communicatie."})
 
 #create an embeddings object
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
@@ -52,6 +50,7 @@ qa = ConversationalRetrievalChain.from_llm(
         model="gpt-3.5-turbo-16k", 
         openai_api_key=openai_api_key), 
         vectorstore.as_retriever(),
+        condense_question_llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo', openai_api_key=openai_api_key, verbose=True), 
         memory=memory,
         verbose=True)
 
