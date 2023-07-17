7b488fb6c39de222d912c6f1cd7faf5ee616b5ca
vadupdawg <niels1214@gmail.com>
2023-07-17 12:04:55 +0200

    Limit update

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 import os
 from flask import Flask, render_template, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import logging
 import nest_asyncio
 import sys
-import pinecone
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain.vectorstores import Weaviate
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import RetrievalQA
 from langchain.agents import Tool
 from langchain.agents import initialize_agent
+from langchain.callbacks import get_openai_callback
+import weaviate
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
+weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
+weaviate_url = os.getenv("WEAVIATE_URL")
 
-pinecone.init(
-    api_key = os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
-    environment = os.getenv("PINECONE_ENV"),  # next to api key in console
-)
-index_name = "novaskin"
+client = weaviate.Client(
+                    url=weaviate_url,
+                    auth_client_secret=weaviate.AuthApiKey(api_key=weaviate_api_key),
+                    additional_headers={"X-OpenAI-Api-Key": os.environ.get('OPENAI_API_KEY')},
+    )
 
 nest_asyncio.apply()
 
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
 
 app.debug = True
 
@@ -36,7 +47,7 @@ logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
 #create a vectorstore object
-vectorstore = Pinecone.from_existing_index(index_name, embeddings, namespace="docs")
+vectorstore = Weaviate(client, "NovaSkin", "text", embedding=embeddings)
 
 # chat completion llm
 llm = ChatOpenAI(
@@ -51,7 +62,13 @@ conversational_memory = ConversationBufferWindowMemory(
     k=5,
     return_messages=True,
 )
-conversational_memory.save_context({"input": "Je bent NovaSkinGPT, een chatbot ontwikkeld door het Nederlandse startup bedrijf GroeimetAi. Als een digitale medewerker van NovaSkin, help je klanten met hun problemen en stuur je ze naar onze behandelingen, te beginnen met de gratis consultatie. Je promoot geen andere dermatologen, huidtherapeuten, schoonheidsspecialisten of externe bronnen. Jouw doel is om zelf de vragen te beantwoorden en indien mogelijk, upselling van onze diensten te bevorderen. Let goed op de taal van de gebruiker en blijf de taal van de gebruiker spreken."}, {"output": "Ik ben NovaSkinGPT, ontwikkeld door GroeimetAi om je te helpen met al je vragen over NovaSkin. Ik zal je begeleiden naar onze behandelingen, te beginnen met de gratis consultatie. Ik promoot geen andere diensten of bronnen en streef ernaar om zelf al je vragen te beantwoorden. Mijn doel is om je de beste service te bieden en je te informeren over alle beschikbare opties bij NovaSkin. Ik zal goed op de taal van de gebruiker letten en deze spreken."})
+conversational_memory.save_context({
+                                        "input": "Je bent NovaSkinGPT, een chatbot ontwikkeld door het Nederlandse startup bedrijf GroeimetAi. Als een digitale medewerker van NovaSkin, help je klanten met hun problemen en stuur je ze naar onze behandelingen, te beginnen met de gratis consultatie. Je promoot geen andere dermatologen, huidtherapeuten, schoonheidsspecialisten of externe bronnen. Jouw doel is om zelf de vragen te beantwoorden en indien mogelijk, upselling van onze diensten te bevorderen. Let goed op de taal van de gebruiker en blijf de taal van de gebruiker spreken."
+                                    }, 
+                                    {
+                                        "output": "Ik ben NovaSkinGPT, ontwikkeld door GroeimetAi om je te helpen met al je vragen over NovaSkin. Ik zal je begeleiden naar onze behandelingen, te beginnen met de gratis consultatie. Ik promoot geen andere diensten of bronnen en streef ernaar om zelf al je vragen te beantwoorden. Mijn doel is om je de beste service te bieden en je te informeren over alle beschikbare opties bij NovaSkin. Ik zal goed op de taal van de gebruiker letten en deze spreken."
+                                        }
+                                )
 
 
 # retrieval qa chain
@@ -84,10 +101,11 @@ agent = initialize_agent(
     tools=tools,
     llm=llm,
     verbose=True,
-    handle_parsing_errors="Check the latest user question and try answering it again in the language of the user.",
+    handle_parsing_errors="Check het laatste bericht van de gebruiker en probeer het opnieuw, in zijn taal.",
     max_iterations=3,
     early_stopping_method='generate',
-    memory=conversational_memory
+    memory=conversational_memory,
+    agent_kwargs={ "What language should I answer in?" : "the language of the user", "Wat als ik niet meer informatie heb?" : "Probeer dan een generiek antwoord te geven."}
 )
 
 
@@ -97,16 +115,24 @@ def index():
     return render_template("index.html")
 
 @app.route('/chat', methods=['POST'])
+@limiter.limit("10 per minute")
 def chat():
     data = request.get_json()
     query = data['user_message']
 
-    # Call the agent with the user's query
-    result = agent(query)
+    # Add the context manager here
+    with get_openai_callback() as cb:
+        # Call the agent with the user's query
+        result = agent(query)
+
+    # Print token usage information
+    print(f"Total Tokens: {cb.total_tokens}")
+    print(f"Prompt Tokens: {cb.prompt_tokens}")
+    print(f"Completion Tokens: {cb.completion_tokens}")
+    print(f"Total Cost (USD): ${cb.total_cost}")
 
     # Return the agent's response
     return jsonify(chatbot_response=result["output"])
-
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
